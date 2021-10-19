6a52a9b6c1ab0ef5182f076ce08da94375982368
Vitaliy <37016779+1k1ru@users.noreply.github.com>
2021-10-19 20:46:17 +0300

    Add config file check

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import toml    
 
 def init_config():
-    global cfg
-    cfg = toml.load('config.toml')
+    try:
+        global cfg
+        cfg = toml.load('config.toml')
+    except:
+        print("Error: Could not find config file.")
+        exit(1)
